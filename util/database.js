4eb14f21d5ffa95627806a8203833bf1f3167977
fanaya <fanaya@bltomato.com>
2020-01-02 18:09:49 -0800

    got DB working

--- util/database.js
+++ util/database.js
@@ -1,17 +1,30 @@
-const MongoClient = require('mongodb').MongoClient;
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const uri = "mongodb+srv://fanaya:6zd7rj63qMinkW1A@cluster0-nmcso.mongodb.net/test?retryWrites=true&w=majority";
+let _db;
 
 const mongoConnect = (callback) => {
-    MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    MongoClient.connect(
+        'mongodb+srv://fanaya:6zd7rj63qMinkW1A@cluster0-nmcso.mongodb.net/test?retryWrites=true&w=majority',
+        {useNewUrlParser: true, useUnifiedTopology: true } 
+    )
     .then(client => {
-        console.log("Connected!");
-        callback(client);
+        console.log('Connected!');
+        _db = client.db();
+        callback();
     })
     .catch(err => {
-        console.log("ERROR!");
         console.log(err);
+        throw err;
     });
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+    if (_db) {
+        return _db;
+    }
+    throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
